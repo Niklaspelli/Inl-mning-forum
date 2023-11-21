095a9c57673f313cc86a7dbe5af88ae2f2943264
Niklas Pelli <niklaspelli22@gmail.com>
2023-11-21 14:25:31 +0100

    Massa förändringar

--- index.js
+++ index.js
@@ -2,38 +2,75 @@ let express = require("express"); // installera express
 let app = express(); // skapa ett express server-objekt
 let port = 8080; // ... som körs på port 8080
 
+
 let httpServer = app.listen(port, function () {
   console.log(`Webbserver körs på port ${port}`); // samma som "Webbserver körs på port " + port
 });
-
-// serva en statisk webbsida (form.html) som innehåller formulär
-app.get("/main", function (req, res) {
     
-  res.sendFile(__dirname + "/main.html");
-  
+app.use(express.urlencoded({ extended: true })); // behövs för att processa data som skickats med POST
+
+let fs = require("fs");
+
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/login.html");
 });
- // hit kommer data när get-formuläret skickas
-app.get("/get-route", function (req, res) {
-  console.log(req.query);
-  let summa = Number(req.query.tal1) + Number(req.query.tal2);
-  console.log(summa);
-  res.send(`${req.query.tal1}+${req.query.tal2}=${summa}`);
-}); 
 
- /* app.post("/checkguestbook",function (req, res) {
-    let inlagg = fs.readfileSync("inlagg.json").toString();
-    inlagg = JSON.parse(inlagg);
-    inlagg.push(req.body);
-    inlagg = JSON.stringify(inlagg);
-    fs.writeFileSync(inlagg);
-    for (i in inlagg) {
-        res.write("<p>" + inlagg[i].author + "<br>" + inlagg.[i].content + "</p>");
+app.post("/checklogin", function (req, res) {
+  let users = JSON.parse(fs.readFileSync("users.json").toString()); // läs in JSON-fil och konvertera till en array med JavaScript-objekt
+  console.log(users);
+  for (i in users) {
+    if (users[i].user == req.body.user && users[i].pass == req.body.pass) {
+      let output = fs.readFileSync("main.html").toString();
+      output = output.replace("***NAMN***", users[i].name);
+      res.send(output);
+      return;
     }
-    res.send();
-    });
+  }
+  let output = fs.readFileSync("login.html").toString();
+  output = output.replace(
+    "<body>",
+    "<body>LOGIN FAILED! PLEASE TRY AGAIN!<br>"
+  );
+  res.send(output);
+});
 
-    app.post("/", function (req, res) {
-        let html = fs.readFileSync("login.html").toString();
-        html = html.replace("***NAME***", req.body.namn);
-        res.send(html);
-      });  */
+
+
+app.get("/",function (req, res) {
+  
+    let inlagg = fs.readFileSync("inlagg.json").toString();
+    inlagg = JSON.parse(inlagg);
+    
+    
+    console.log(inlagg);
+    let output = "";
+    for (let i = 0; i < inlagg.length;) {
+      output += `<p>${inlagg[i].author} skriver: ${inlagg[i].content}</p>`;
+    }
+    let html = fs.readFileSync("main.html").toString();
+    html = html.replace("***GÄSTER***", output);
+    res.send(html);
+});
+
+app.post("/", function (req, res) {
+  let inlagg = fs.readFileSync("inlagg.json").toString();
+  inlagg = JSON.parse(inlagg);
+
+  inlagg.push(req.body);
+
+  let json_text = JSON.stringify(inlagg);
+  console.log(json_text);
+  fs.writeFileSync("inlagg.json", json_text);
+
+  let output = "";
+  for (let i = 0; i < inlagg.length; i++) {
+    output += `<p><b>${inlagg[i].author} </b> från ${inlagg[i].zip} skriver: <br> <em> ${inlagg[i].content}</em> </p>`
+  }
+  let html = fs.readFileSync("main.html").toString();
+  html = html.replace("***GÄSTER***", output);
+  res.send(html);
+location.reload();
+
+});
+
